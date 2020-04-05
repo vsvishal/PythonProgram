1efadcc43d9db914632880d4272b3744608660ff
vsvishal11 <vsvishal11@yahoo.com>
2020-04-05 17:53:00 +0530

    initial commit

--- PDFToCsvTabulaLib.py
+++
@@ -1,9 +0,0 @@
-import tabula
-
-#Read PDF into list of DataFrame
-df = tabula.read_pdf(r"C:\Users\VS\Desktop\191640003.pdf", pages="all")
-
-#Convert PDF into CSV file
-tabula.convert_into(r"C:\Users\VS\Desktop\191640003.pdf", "Output.csv", output_format="csv", pages='all')
-
-
